804540dec012fdd9b53877e38b88339aab8fb8e5
arundhati-shreya <arundhati0426@gmail.com>
2024-01-21 20:55:45 +0530

    Fixing the Cart problems

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification';
+
+import { fetchCartData,sendCartData } from './store/cart-actions';
 
 let isInitial = true;
 
@@ -15,50 +16,19 @@ function App() {
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
 
+  useEffect(()=>{
+    dispatch(fetchCartData())
+  },[dispatch]);
+
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
-      const response = await fetch(
-        ' https://e-commerce-327da-default-rtdb.firebaseio.com/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-        })
-      );
-    };
-
-    if (isInitial) {
-      isInitial = false;
+    if(isInitial){
+      isInitial=false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-    });
+    if(cart.changed){
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
